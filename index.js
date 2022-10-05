165184d5b6dd2880f455429e5485a12d2e069677
Raphael Serota <raphael.serota@gmail.com>
2022-10-05 15:06:35 -0500

    basic setup

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static('frontend/dist'));
 let todos = [{id: 1, name: 'Cohen Interview Assignment'}];
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+	res.sendFile('./frontend/dist/index.html', {root: './'});
+})
 app.get('/todos', (req, res) => {
-  return res.status(200).send(todos);
+	return res.status(200).send(todos);
 });
 
 app.listen(PORT, () =>
-  console.log(`Example app listening on port ${PORT}!`),
+	console.log(`Example app listening on port ${PORT}!`)
 );
